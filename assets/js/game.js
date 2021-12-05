62a19615eb1cc4cef2c7a2f98f1b2378f1a1f7cf
matthewpetriella <93597207+matthewpetriella@users.noreply.github.com>
2021-12-04 19:18:37 -0500

    built intro and first attack of each opponent

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,32 @@
-// this creates a function named "fight"
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logas a string, good for leaving youself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10)
+var fight = function () {
+    //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!")
 
-console.log("Our robot's name is " + playerName);
+    //Subtract playerAttack from enemyHealth > update enemyHealth variable
+    enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + " for " + playerAttack + " damage. " + enemyName + " now has " + enemyHealth + " health remaining. "
+    );
 
-// fight();
+    //Subtract enemyAttack from playerHealth > update playerHealth variable
+    playerHealth = playerHealth - enemyAttack;
 
+    //Log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + "for " + enemyAttack + " damage. " + playerName + " now has " + playerHealth + " health remaining. "
+    );
+};
+
+fight();
